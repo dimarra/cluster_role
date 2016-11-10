a20377889ae256e2d10b349ecd8e88830859e56e
Raul Dimar <rdimar@shelde.com>
2016-11-10 13:28:43 +1100

    configure the timezone

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,7 @@ class cluster_role (
   
     $cluster      = undef,
     $role         = undef,
+    $timezone     = undef,
   
 ) {
   
@@ -28,4 +29,8 @@ class cluster_role (
     ensure  => file,
     content  => "cluster: $cluster \nrole: $role \n",
   }
+  
+  class { 'timezone':
+    timezone => $timezone,
+}
 }
